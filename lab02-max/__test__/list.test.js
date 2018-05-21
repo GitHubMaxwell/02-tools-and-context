881f44b14807d0e72995116fa0bc7dbd89aa7df9
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-05-21 16:19:43 -0700

    working on map function

--- lab02-max/__test__/list.test.js
+++ lab02-max/__test__/list.test.js
@@ -1,39 +1,66 @@
 'use strict';
-let List = require('../lib/list.js');
+let list = require('../lib/list.js');
 
 describe('List Module', () => {
 
-  it('push() returns the length of the list', () => {
-    let myList = new List();
-    myList.push('FOO');
-    expect(myList.length).toBe(1);
+  // it('push() returns array filled with new items', () => {
+  //   let myList = new List();
+  //   //trying to compare this javscript class with an array which arent the same
+  //   myList.push(1);
+  //   expect(myList.length).toBe(1);
+  // });
+
+  //   it('push() returns the length of the list', () => {
+  //     let myList = new List();
+  //     myList.push('FOO');
+  //     expect(myList.length).toBe(1);
+  //   });
+
+  //   it('push() adds an element to the end of a list', () => {
+  //     let myList = new List();
+  //     myList.push('FOO');
+  //     expect(myList[0]).toBe('FOO');
+  //   });
+
+  // it('pop() should return undefined if the list is empty', () => {
+  //   let myList = new List();
+  //   expect(myList.pop()).toBeUndefined();
+  // });
+
+  // it('pop() removes an element from the end of a list', () => {
+  //   let myList = new List();
+  //   myList.push('a');
+  //   myList.push('b');
+  //   expect(myList.pop()).toBe('b');
+  // });
+
+  //   it('forEach() function should return undefined', () => {
+  //     let myList = new List();
+  //     // (myList.toArray())
+  //     expect(myList.forEach()).toBeUndefined();
+  //   });
+
+  // it('forEach() array should equal the source value inputted', () => {
+  //   let source = [1,2,3,4];
+  //   let myList = new List();
+  //   myList.forEach(source);
+  // (myList.toArray());
+  // expect(myList.forEach([1, 2, 3, 4])).toEqual([1,2,3,4]);
+  //   expect(myList).toEqual([1,2,3,4]);
+  // });
+
+  //   it('forEach() array should equal', () => {
+  //     // let source = [1,2,3,4];
+  //     let myList = new List();
+  //     // (myList.toArray());
+  //     // expect(myList.forEach([1, 2, 3, 4])).toEqual([1,2,3,4]);
+  //     expect(myList.toArray()).toEqual([1,2,3,4]);
+  //   });
+
+  it('map() array should equal "this is 1"', () => {
+    let myList = new list.List();
+    // let newList = list.toArray(myList.push(1));
+    myList.push(1);
+    expect(myList.map(list.logging)).toEqual([`this is ${1}`]);
   });
-
-//   it('push() adds an element to the end of a list', () => {
-//     let myList = new List();
-//     myList.push('FOO');
-//     expect(myList[0]).toBe('FOO');
-//   });
-
-//   it('pop() should return undefined if the list is empty', () => {
-//     let myList = new List();
-//     expect(myList.pop()).toBeUndefined();
-//   });
-
-//   it('pop() removes an element from the end of a list', () => {
-//     let myList = new List();
-//     myList.push('a');
-//     myList.push('b');
-//     expect(myList.pop()).toBe('b');
-//   });
-
-//   it('forEach() function should return undefined', () => {
-//     let myList = new List();
-//     expect(myList.forEach()).toBeUndefined();
-//   });
-
-//   it('forEach() array should be unchanged', () => {
-//     let myList = new List();
-//     expect(myList.forEach([1, 2, 3, 4])).toBe([1, 2, 3, 4]);
-//   });
 });
--- lab02-max/lib/list.js
+++ lab02-max/lib/list.js
@@ -2,37 +2,82 @@
 class List {
 
   constructor() {
+    //   constructor(array = []) {
     this.length = 0;
+    // if (!array) {
+    //   return undefined;
+    // }
+    // this.length = array.length;
+    
+    //want to populate the list with itme in array
+
+    // for(let i = 0; i < array.length; i++) {
+    //   this[i] = array[i];
+    // }
   }
 
+  map(func) {
+    let newArr =[];
+    for (let i = 0; i < this.length; i++) {
+      newArr.push(func(this,i));
+    }
+    console.log(newArr);
+    return newArr;
+  }
+
+  // logging(arr, i) {
+  //   return console.log(`this is ${arr[i]}`);
+  // }
+  
   push(item) {
     this[this.length++] = item;
     return this.length;
   }
-  
-//   pop() {
-//     let remove = this[this.length - 1];
-//     this.length--;
-//     if(this.length < 0) {
+
+  toArray(array) {
+  // convert object list to JS array
+  // start at zero and end at 
+    for(let i = 0; i < this.length; i++) {
+      array[i] = this[i];
+    }
+    return array;
+  }
+
+  // pop() {
+  //   let remove = this[this.length - 1];
+  //   this.length--;
+  //   if(this.length < 0) {
+  //     return undefined;
+  //   }
+  //   else {
+  //     delete this[this.length];
+  //   }
+  //   return remove;
+  // }
+
+//   forEach(array) {
+//     if(!array) {
 //       return undefined;
 //     }
-//     else {
-//       delete this[this.length];
+//     let newArray = [];
+//     for(let i = 0; i < array.length; i++) {
+//       newArray.length++;
+//       newArray[i] = array[i];
 //     }
-//     return remove;
-//   }
-//   forEach(array) {
-//     this.length = array;
-//   }
-//   reduce(array) {
-//     this.length = array;
+//     return newArray;
 //   }
 //   map(array) {
-//     this.length = array;
+//     
 //   }
 //   filter(array) {
-//     this.length = array;
+//     
 //   }
+//   reduce(array) {
+//     
+//   }
+
 }
 
-module.exports = List;
+module.exports = exports = {};
+exports.List = List;
+exports.logging = (arr,i) => {return console.log(`this is ${arr[i]}`);};
